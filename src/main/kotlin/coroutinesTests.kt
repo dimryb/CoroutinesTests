2cafea7059a74ffc469ab07360e42aa5dd5e9444
dmitry <dimryb@bk.ru>
2022-10-13 22:29:12 +0700

    Проверка cancelAndJoin

---
+++ src/main/kotlin/coroutinesTests.kt
@@ -0,0 +1,17 @@
+import kotlinx.coroutines.*
+import kotlin.coroutines.*
+
+fun main() = runBlocking {
+    val job = CoroutineScope(EmptyCoroutineContext).launch {
+        launch {
+            delay(500)
+            println("ok") // <--
+        }
+        launch {
+            delay(500)
+            println("ok")
+        }
+    }
+    delay(100)
+    job.cancelAndJoin()
+}
